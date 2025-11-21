e233961438816dc0b93442170dd0222834bdd2bc
haelin kim <84735609+haeffari@users.noreply.github.com>
2025-11-21 22:36:07 +0900

    Add files via upload

---
+++ slides.js
@@ -0,0 +1,59 @@
+// slides.js
+const slides = [
+  {
+    slug: "pinecut",
+    slideTitle: "Pineapple Enzyme",
+    slideDescription:
+      "A fresh and energetic product visual, highlighting the vitality of pineapple enzyme nutrition.",
+    slideTags: ["scroll wheel"],
+    slideVideo: "./projects_img/slide_video1.mp4",
+    modalImg: "./projects_img/project_pinecut.gif"
+  },
+  {
+    slug: "shampoobar",
+    slideTitle: "Nature Rocks Bar",
+    slideDescription:
+      "A minimal yet organic concept emphasizing sustainability, gentle care, and eco-friendly living.",
+    slideTags: ["scroll wheel"],
+    slideVideo: "./projects_img/slide_video2.mp4",
+    modalImg: "./projects_img/project_shampoo.jpg"
+  },
+  {
+    slug: "toner",
+    slideTitle: "Coral Refresh Toner",
+    slideDescription:
+      "A serene and hydrating skincare visual inspired by coral freshness and marine purity.",
+    slideTags: ["scroll wheel"],
+    slideVideo: "./projects_img/slide_video3.mp4",
+    modalImg: "./projects_img/project_toner.jpg"
+  },
+  {
+    slug: "salad",
+    slideTitle: "Organic Fresh Salad",
+    slideDescription:
+      "A vibrant food composition combining crisp textures and subtle motion to convey natural freshness.",
+    slideTags: ["scroll wheel"],
+    slideVideo: "./projects_img/slide_video4.mp4",
+    modalImg: "./projects_img/project_salad.jpg"
+  },
+  {
+    slug: "banner",
+    slideTitle: "Event Banner",
+    slideDescription:
+       "A bold and engaging banner layout designed to deliver key event messages with clarity and impact.",
+    slideTags: ["scroll wheel"],
+    slideVideo: "./projects_img/slide_video5.mp4",
+    modalImg: "./projects_img/project_banner.jpg"
+  },
+  {
+    slug: "video",
+    slideTitle: "Short Form Video",
+    slideDescription:
+       "A set of short-form visuals crafted across pet, podcast, and informational themes for dynamic storytelling.",
+    slideTags: ["scroll wheel"],
+    slideVideo: "./projects_img/slide_video6.mp4",
+    modalImg: "./projects_img/project_video.mp4"
+  }
+];
+
+export default slides;
